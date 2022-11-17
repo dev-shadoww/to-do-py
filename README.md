df1dd3948c533d7778b6bb21f3349dbbd8f3b81f
shadoww <dev.shadoww@gmail.com>
2022-11-17 13:27:02 +0530

    Update README.md and Fix bugs

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# TO-DO-PY
+# to-do-py
 
-A to-do program created in python mainly for terminal.Still in initial stage adding new features as i learn more concepts and get more ideas.
+To-do-py is similar to a **to-do** app created for terminal in python.
 
-## Problems
-
-1. When open() is used and if the file doen't exist then it should create it but here it's giving FileNotFoundError so the two additional files are included with it.
-
-![o-do-py](./to-do-py-img.png)
+![working of to-do-py](./to-do-py.gif)
--- to_do.py
+++ to_do.py
@@ -1,25 +1,43 @@
-print("Welcome to to-do-py")
-username = input("Enter your username : ")
-with open('./to-do-py-usernames.txt', 'r+') as userfile:
-    users = userfile.readline()
-    if username not in users:
-        print(f"Hi, {username}!")
-        userfile.write(username)
-    else:
-        print(f"Welcome back, {username}")
-user_choice = input(
-    "To add new task enter 'a' ,to open your tasks enter 'o' or to quit enter 'q' : ")
-while user_choice == 'a' or user_choice == 'o':
-    if user_choice == 'a':
-        with open('./to-do-tasks.txt', 'a') as to_do_task_file:
-            task = input("Enter the task : ")
-            to_do_task_file.write(f"{task}\n")
-    elif user_choice == 'o':
-        with open('./to-do-tasks.txt', 'r') as to_do_task_file:
-            to_do_tasks = to_do_task_file.readlines()
-            index_of_task = 1
-            for line in to_do_tasks:
-                print(f"{index_of_task}. {line.strip()}")
-                index_of_task += 1
-    user_choice = input(
-        "To add new task enter 'a' ,to open your tasks enter 'o' or to quit enter 'q' : ")
+import os.path
+
+print("🙇 Welcome to to-do-py,\n")
+userName = input("What is your name ?\n> ")
+if(os.path.isfile('./tdp_username.txt')):
+    with open('./tdp_username.txt', 'a+') as userfile:
+        users = userfile.readline()
+        if userName not in users:
+            print(f"Hello 👋, {userName}!")
+            userfile.write(f"{userName}")
+        else:
+            print(f"Hello 👋, {userName}! Nice to see you again,")
+else:
+    with open('./tdp_username.txt', 'w+') as userfile:
+        users = userfile.readline()
+        if userName not in users:
+            print(f"Hello 👋, {userName}!")
+            userfile.write(f"{userName}")
+        else:
+            print(f"Hello 👋, {userName}! Nice to see you again,")
+
+print("\n🔢 Options :\n1. Press 🇦  to add new task.\n2. Press 🇴  to display tasks.\n3. Press 🇶  to quit.\n\n")
+userChoice = input("So what do you want to do ? 👇,\n> ").lower()
+
+while userChoice == 'a' or userChoice == 'o':
+    if userChoice == 'a':
+        if(os.path.isfile('./tdp_tasks.txt')):
+            with open('./tdp_tasks.txt', 'a') as tdpFile:
+                task = input("Enter the task 👇,\n> ")
+                tdpFile.write(f"{task}\n")
+        else:
+            with open('./tdp_tasks.txt', 'w') as tdpFile:
+                task = input("Enter the task 👇,\n> ")
+                tdpFile.write(f"{task}\n")
+    elif userChoice == 'o':
+        with open('./tdp_tasks.txt', 'r') as tdpFile:
+            tdp_tasks = tdpFile.readlines()
+            numberOfTasks = 1
+            for line in tdp_tasks:
+                print(f"{numberOfTasks}. {line.strip()}")
+                numberOfTasks += 1
+    print("\n🔢 Options :\n1. Press 🇦  to add new task.\n2. Press 🇴  to display tasks.\n3. Press 🇶  to quit.\n\n")
+    userChoice = input("So what do you want to do ? 👇,\n> ")
