155d20121344682e0d5254141a62627f1103a5e4
shadoww <dev.shadoww@gmail.com>
2022-11-17 20:06:38 +0530

    Refactor code

--- to_do.py
+++ to_do.py
@@ -1,37 +1,18 @@
-import os.path
-
 print("🙇 Welcome to to-do-py,\n")
 userName = input("What is your name ?\n> ")
-if(os.path.isfile('./tdp_username.txt')):
-    with open('./tdp_username.txt', 'a+') as userfile:
-        users = userfile.readline()
-        if userName not in users:
-            print(f"Hello 👋, {userName}!")
-            userfile.write(f"{userName}")
-        else:
-            print(f"Hello 👋, {userName}! Nice to see you again,")
-else:
-    with open('./tdp_username.txt', 'w+') as userfile:
-        users = userfile.readline()
-        if userName not in users:
-            print(f"Hello 👋, {userName}!")
-            userfile.write(f"{userName}")
-        else:
-            print(f"Hello 👋, {userName}! Nice to see you again,")
+with open('./tdp_username.txt', 'a+') as userfile:
+    users = userfile.readline()
+    print(f"Hello 👋, {userName}!")
+    userfile.write(f"{userName}")
 
 print("\n🔢 Options :\n1. Press 🇦  to add new task.\n2. Press 🇴  to display tasks.\n3. Press 🇶  to quit.\n\n")
 userChoice = input("So what do you want to do ? 👇,\n> ").lower()
 
 while userChoice == 'a' or userChoice == 'o':
     if userChoice == 'a':
-        if(os.path.isfile('./tdp_tasks.txt')):
-            with open('./tdp_tasks.txt', 'a') as tdpFile:
-                task = input("Enter the task 👇,\n> ")
-                tdpFile.write(f"{task}\n")
-        else:
-            with open('./tdp_tasks.txt', 'w') as tdpFile:
-                task = input("Enter the task 👇,\n> ")
-                tdpFile.write(f"{task}\n")
+        with open('./tdp_tasks.txt', 'a+') as tdpFile:
+            task = input("Enter the task 👇,\n> ")
+            tdpFile.write(f"{task}\n")
     elif userChoice == 'o':
         with open('./tdp_tasks.txt', 'r') as tdpFile:
             tdp_tasks = tdpFile.readlines()
